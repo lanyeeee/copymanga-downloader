e86a52fd414467b5aedf185b558e84c9174e5a30
lanyeeee <1210347077@qq.com>
2024-11-22 20:30:17 +0800

    feat: 用`struct`代替`type`，以便`tauri-specta`能正确识别

--- src-tauri/src/responses/search_resp_data.rs
+++ src-tauri/src/responses/search_resp_data.rs
@@ -1,9 +1,26 @@
+use std::ops::{Deref, DerefMut};
+
 use serde::{Deserialize, Serialize};
 use specta::Type;
 
 use super::{AuthorRespData, Pagination};
 
-pub type SearchRespData = Pagination<ComicInSearchRespData>;
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Type)]
+pub struct SearchRespData(Pagination<ComicInSearchRespData>);
+
+impl Deref for SearchRespData {
+    type Target = Pagination<ComicInSearchRespData>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for SearchRespData {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Type)]
 #[serde(rename_all = "camelCase")]
