176264b88eec9473c4d737ddfa49dfda67f23d2c
lanyeeee <lanyeeeeee@gmail.com>
2025-05-28 18:44:26 +0800

    fix: 修复无法登录和获取用户信息的问题

--- src-tauri/src/copy_client.rs
+++ src-tauri/src/copy_client.rs
@@ -51,8 +51,6 @@ impl CopyClient {
         let http_resp = self
             .api_client
             .post(format!("https://{api_domain}/api/v3/register"))
-            .header("version", "2.2.5")
-            .header("source", "copyApp")
             .form(&form)
             .send_with_timeout_msg()
             .await?;
@@ -174,17 +172,6 @@ impl CopyClient {
             .api_client
             .get(format!("https://{api_domain}/api/v3/search/comic"))
             .query(&params)
-            .header("User-Agent", "COPY/2.2.5")
-            .header("Accept", "application/json")
-            .header("Accept-Encoding", "gzip")
-            .header("source", "copyApp")
-            .header("deviceinfo", "DCO-AL00-DCO-AL00")
-            .header("webp", "1")
-            .header("authorization", "Token")
-            .header("platform", "4")
-            .header("referer", "com.copymanga.app-2.2.5")
-            .header("version", "2.2.5")
-            .header("region", "1")
             .send_with_timeout_msg()
             .await?;
         // 检查http响应状态码
@@ -224,17 +211,6 @@ impl CopyClient {
                 "https://{api_domain}/api/v3/comic2/{comic_path_word}"
             ))
             .query(&params)
-            .header("User-Agent", "COPY/2.2.5")
-            .header("Accept", "application/json")
-            .header("Accept-Encoding", "gzip")
-            .header("source", "copyApp")
-            .header("deviceinfo", "DCO-AL00-DCO-AL00")
-            .header("webp", "1")
-            .header("authorization", "Token")
-            .header("platform", "4")
-            .header("referer", "com.copymanga.app-2.2.5")
-            .header("version", "2.2.5")
-            .header("region", "1")
             .send_with_timeout_msg()
             .await?;
         // 检查http响应状态码
@@ -321,17 +297,6 @@ impl CopyClient {
         let http_resp = self.api_client
             .get(format!("https://{api_domain}/api/v3/comic/{comic_path_word}/group/{group_path_word}/chapters"))
             .query(&params)
-            .header("User-Agent", "COPY/2.2.5")
-            .header("Accept", "application/json")
-            .header("Accept-Encoding", "gzip")
-            .header("source", "copyApp")
-            .header("deviceinfo", "DCO-AL00-DCO-AL00")
-            .header("webp", "1")
-            .header("authorization", "Token")
-            .header("platform", "4")
-            .header("referer", "com.copymanga.app-2.2.5")
-            .header("version", "2.2.5")
-            .header("region", "1")
             .send_with_timeout_msg()
             .await?;
         // 检查http响应状态码
@@ -395,17 +360,8 @@ impl CopyClient {
                 "https://{api_domain}/api/v3/comic/{comic_path_word}/chapter2/{chapter_uuid}"
             ))
             .query(&params)
-            .header("User-Agent", "COPY/2.2.5")
-            .header("Accept", "application/json")
-            .header("Accept-Encoding", "gzip")
-            .header("source", "copyApp")
-            .header("deviceinfo", "DCO-AL00-DCO-AL00")
             .header("webp", "0")
             .header("authorization", authorization)
-            .header("platform", "4")
-            .header("referer", "com.copymanga.app-2.2.5")
-            .header("version", "2.2.5")
-            .header("region", "1")
             .send_with_timeout_msg()
             .await?;
         // 检查http响应状态码
@@ -473,17 +429,7 @@ impl CopyClient {
             .api_client
             .get(format!("https://{api_domain}/api/v3/member/collect/comics"))
             .query(&params)
-            .header("User-Agent", "COPY/2.2.5")
-            .header("Accept", "application/json")
-            .header("Accept-Encoding", "gzip")
-            .header("source", "copyApp")
-            .header("deviceinfo", "DCO-AL00-DCO-AL00")
-            .header("webp", "1")
             .header("authorization", authorization)
-            .header("platform", "4")
-            .header("referer", "com.copymanga.app-2.2.5")
-            .header("version", "2.2.5")
-            .header("region", "1")
             .send_with_timeout_msg()
             .await?;
         // 检查http响应状态码
@@ -542,12 +488,28 @@ fn create_img_client() -> ClientWithMiddleware {
 }
 
 fn create_api_client() -> ClientWithMiddleware {
+    use reqwest::header::{HeaderMap, HeaderValue};
     let retry_policy = ExponentialBackoff::builder()
         .base(1) // 指数为1，保证重试间隔为1秒不变
         .jitter(Jitter::Bounded) // 重试间隔在1秒左右波动
         .build_with_total_retry_duration(Duration::from_secs(5)); // 重试总时长为5秒
 
+    let to_header_value = HeaderValue::from_static;
+
+    let mut headers = HeaderMap::new();
+    headers.insert("User-Agent", to_header_value("COPY/2.2.5"));
+    headers.insert("Accept", to_header_value("application/json"));
+    headers.insert("Accept-Encoding", to_header_value("gzip"));
+    headers.insert("source", to_header_value("copyApp"));
+    headers.insert("deviceinfo", to_header_value("DCO-AL00-DCO-AL00"));
+    headers.insert("webp", to_header_value("1"));
+    headers.insert("platform", to_header_value("4"));
+    headers.insert("referer", to_header_value("com.copymanga.app-2.2.5"));
+    headers.insert("version", to_header_value("2.2.5"));
+    headers.insert("region", to_header_value("1"));
+
     let client = reqwest::ClientBuilder::new()
+        .default_headers(headers)
         .timeout(Duration::from_secs(3)) // 每个请求超过3秒就超时
         .build()
         .unwrap();
